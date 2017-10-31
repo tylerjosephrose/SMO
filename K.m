f16ef08c7fe76b4f1cfd5d58baae325c2209abd2
roset3 <roset3@mail.uc.edu>
2017-10-31 13:17:01 -0400

    Delete K.m

--- K.m
+++
@@ -1,7 +0,0 @@
-% This is the Kernel function which will apply the dot product of the two
-% terms from the dataset
-% Seth Dippold and Tyler Rose
-function ker = K( Xi, Xj)
-ker = dot(Xi,Xj);
-return
-end
