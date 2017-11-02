93794ae36f166391d99a8624c7e5702508035d4a
dippolsp <dippolsp@mail.uc.edu>
2017-11-02 13:47:28 -0400

    Update HW5.m
    did things

--- HW5.m
+++ HW5.m
@@ -18,7 +18,7 @@ for i=1:L
        y(i) = -1;
    end
 end
-C = 0.5;
+C = 1;
 tol = 10e-5;
 
 % Initialize alpha constrained to the sum of y*alpha = 0 & alpha >= 0
@@ -29,69 +29,94 @@ for i=1:L
 end
 totalSum = sum(y.*alpha);
 if totalSum > 0
-    place = find(-1)
+    place = find(-1);
     alpha(place) = totalSum + alpha(place);
 elseif totalSum < 0
-    place = find(1)
+    place = find(1);
     alpha(place) = -totalSum + alpha(place);
 end
+bias = 0;
+count = 0;
+while count < 20
+    L = size(meas,1);
+    % Calculate the weight vector
+    w = (alpha.*y).'*x;
 
-% Initialize b to 0
-b = 0;
+    % Calculate KKT Conditions
+    KKT = zeros(L,1);
+    for i=1:L
+        KKT(i) = alpha(i)*(y(i)*(dot(w,x(i,:) + bias) - 1));
+    end
+    % Pick x1 and x2
+    [~,i1] = max(KKT);
+    x1 = x(i1,:);
+    %Ei = sum(alpha.*y.*(dot(x(,
+    E = zeros(L,1);
+    for i=1:L
+        for j=1:L
+            E(i) = E(i) + alpha(j)*y(j)*(dot(x(j),x(1)) - dot(x(j),x(i))) + y(i)- y(1)*dot(x(i),x(j));
+        end
+    end
+    [~,i2] = max(abs(E));
+    x2 = x(i2,:);
 
-% loop
-% Calculate the weight vector
-w = (alpha.*y).'*x;
+    if y(i1)~=y(i2)
+        L=max(0,alpha(i2)-alpha(i1));
+        H=min(C,C+alpha(i2)-alpha(i1));
+    else 
+        L=max(0,alpha(i1)+alpha(i2)-C);
+        H=min(C,alpha(i1)+alpha(i2));
+    end
 
-% Calculate KKT Conditions
-KKT = zeros(L,1);
-for i=1:L
-    KKT(i) = alpha(i)*(y(i)*(dot(w,x(i,:) + b) - 1));
+
+    % Calculate k
+    k  = dot(x1,x1) + dot(x2,x2) - 2*dot(x1,x2);
+%     if k<=0
+%         continue
+%     end
+    % update alphas
+    alpha1_old = alpha(i1);
+    alpha2_old = alpha(i2);
+    alpha(i2) = alpha2_old + (y(i2)*E(i2))/k;
+    alpha(alpha < L)=L;
+    alpha(alpha > H)=H;
+    alpha(i1) = alpha1_old +y(i1)*y(i2)*(alpha2_old - alpha(i2));
+
+
+
+    b1 = bias - E(i1) - y(i1)*(alpha(i1)-alpha1_old)*x(i1,:)*x(i1,:)' - y(i2)*(alpha(i2)-alpha2_old)*x(i1,:)*x(i2,:)';
+    b2 = bias - E(i2) - y(i1)*(alpha(i1)-alpha1_old)*x(i1,:)*x(i2,:)' - y(i2)*(alpha(i2)-alpha2_old)*x(i2,:)*x(i2,:)';
+
+    if 0<alpha(i1)<C
+        bias=b1;
+    elseif 0<alpha(i2)<C
+        bias=b2;
+    else
+        bias=(b1+b2)/2;
+    end
+    
+%     if(alpha(i2) == alpha2_old && alpha(i1) == alpha1_old)
+%         break;
+%     end
+    
+    fprintf('count, %i\n', count)
+    count=count+1;
 end
-% Pick x1 and x2
-[~,i1] = max(KKT);
-x1 = x(i1,:);
-%Ei = sum(alpha.*y.*(dot(x(,
-E = zeros(L,1);
-for i=1:L
-    for j=1:L
-        E(i) = E(i) + alpha(j)*y(j)*(dot(x(j),x(1)) - dot(x(j),x(i))) + y(i)- y(i)*dot(x(i),x(j));
+
+
+countDelicious = 0;
+countNasty = 0;
+for k = 1:150
+    result = bias + sum(x(k).*w)
+    if(result >0 && y(k) > 0)
+        countDelicious = countDelicious + 1;
+    elseif(result<0 && y(k) <0)
+        countDelicious = countDelicious + 1;
+    else
+        countNasty = countNasty + 1;
     end
 end
-[~,i2] = max(E);
-x2 = x(i2,:);
-
-if y(i1)~=y(i2)
-    L=max(0,alpha(i2)-alpha(i1));
-    H=min(C,C+alpha(i2)-alpha(i1));
-else 
-    L=max(0,alpha(i1)+alpha(i2)-C);
-    H=min(C,alpha(i1)+alpha(i2));
-end
 
 
-% Calculate k
-k  = dot(x1,x1) + dot(x2,x2) - 2*dot(x1,x2);
-% if k<=0
-%     continue
-% end
-% update alphas
-alpha1_old = alpha(i1);
-alpha2_old = alpha(i2);
-alpha(i2) = alpha2_old + (y(i2)*E(i2))/k;
-alpha(i1) = alpha1_old +y(i1)*y(i2)*(alpha2_old - alpha(i2));
 
-alpha(alpha < L)=L;
-alpha(alpha > H)=H;
-
-b1 = b - E(i1) - y(i1)*(alpha(i1)-alpha1_old)*x(i1,:)*x(i1,:)' - y(i2)*(alpha(i2)-alpha2_old)*x(i1,:)*x(i2,:)';
-b2 = b - E(i2) - y(i1)*(alpha(i1)-alpha1_old)*x(i1,:)*x(i2,:)' - y(i2)*(alpha(i2)-alpha2_old)*x(i2,:)*x(i2,:)';
-
-if 0<alpha(i1)<C
-    bias=b1;
-elseif 0<alpha(i2)<C
-    bias=b2;
-else
-    bias=(b1+b2)/2;
-end
-
+total= sum(y.*alpha)
